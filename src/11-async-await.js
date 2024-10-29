942a9da6956eab20ddc8f72f7111b9001c2c8384
Guideon <gbojorge@ccss.sa.cr>
2024-10-29 11:24:21 -0600

    -fetch y async

---
+++ src/11-async-await.js
@@ -0,0 +1,24 @@
+
+const getImage = async() =>{
+    try {
+        
+    
+        const apiKey='UkdomiVzNarj4C6wsCT2cnKcfJ61e1e1';
+    
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    
+        const {data} = await resp.json();
+        
+        const {url} = data.images.original;
+        
+        const img =document.createElement('img');
+    
+        img.src = url;
+    
+        document.body.append(img);
+    } catch (error) {
+     console.error(error);
+    }    
+    }
+    
+    getImage();
